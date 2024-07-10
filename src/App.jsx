05b91db3e82180951245e2a3573d3104f7763430
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-07-10 12:22:20 +0200

    staging

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { Fragment } from "react/jsx-runtime";
 import "./App.css";
-import data from "./assets/data.json";
-import { useState } from "react";
-
-function ProductsList({ products, setProducts }) {
-  const handleClick = (product) => {
-    setProducts(products.filter((p) => p.id !== product.id));
-  };
-  return (
-    <div className="products__list">
-      {products.map((product) => (
-        <Fragment key={product.id}>
-          <p> {product.title} </p>
-          <button onClick={() => handleClick(product)}> remove </button>
-        </Fragment>
-      ))}
-    </div>
-  );
-}
+import MovieList from "./components/MovieList";
 
 export default function App() {
-  const [products, setProducts] = useState(data);
-
   return (
-    <ul className="App">
-      <ProductsList products={products} setProducts={setProducts} />
-    </ul>
+    <div className="App">
+      <MovieList />
+    </div>
   );
 }
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import moviesData from "../movies-data.json";
+import MovieCard from "./MovieCard";
+
+export default function MovieList() {
+  const [movies, setMovies] = useState(moviesData);
+
+  const deleteMovie = (movieId) => {
+    const filteredMovies = movies.filter((movie) => {
+      return movie._id !== movieId;
+    });
+
+    setMovies(filteredMovies);
+  };
+
+  return (
+    <div>
+      <h2>Movie List</h2>
+      {movies.map((movie) => {
+        return (
+          <MovieCard
+            key={movie._id}
+            movie={movie}
+            clickToDelete={deleteMovie}
+          />
+        );
+      })}
+    </div>
+  );
+}
